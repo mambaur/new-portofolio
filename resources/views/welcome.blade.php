01c40e38915b6d0e86e0a2703b2f5986c72ddaee
mambaur <alwiroziq1998@gmail.com>
2022-06-06 23:39:46 +0700

    add image portofolio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,19 +75,20 @@
 							<div class="media-body">
 								<div class="personal_text">
 									<h6>Hello Everybody, i am</h6>
-									<h3>Donald McKinney</h3>
-									<h4>Junior UI/UX Developer</h4>
+									<h3>Bauroziq</h3>
+									<h4>Software Developer</h4>
 									<p>You will begin to realise why this exercise is called the Dickens Pattern (with reference to the ghost showing Scrooge some different futures)</p>
 									<ul class="list basic_info">
-										<li><a href="#"><i class="lnr lnr-calendar-full"></i> 31st December, 1992</a></li>
-										<li><a href="#"><i class="lnr lnr-phone-handset"></i> 44 (012) 6954 783</a></li>
-										<li><a href="#"><i class="lnr lnr-envelope"></i> businessplan@donald</a></li>
-										<li><a href="#"><i class="lnr lnr-home"></i> Santa monica bullevard</a></li>
+										<li><a href="#"><i class="lnr lnr-calendar-full"></i> 17th August, 1998</a></li>
+										<li><a href="#"><i class="lnr lnr-phone-handset"></i> +62 856 0495 9785</a></li>
+										<li><a href="#"><i class="lnr lnr-envelope"></i> bauroziq@gmail.com</a></li>
+										<li><a href="#"><i class="lnr lnr-home"></i> Jember Indonesia</a></li>
 									</ul>
 									<ul class="list personal_social">
-										<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-										<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+										<li><a href="https://www.facebook.com/alwi.roziq"><i class="fa fa-facebook"></i></a></li>
+										<li><a href="https://twitter.com/bauroziq"><i class="fa fa-twitter"></i></a></li>
+										<li><a href="https://www.linkedin.com/in/mambaur-roziq-605962167/"><i class="fa fa-linkedin"></i></a></li>
+										<li><a href="https://www.instagram.com/bauroziq/"><i class="fa fa-instagram"></i></a></li>
 									</ul>
 								</div>
 							</div>
@@ -105,7 +106,7 @@
         			<div class="col-lg-6">
         				<div class="welcome_text">
         					<h4>About Myself</h4>
-        					<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed.</p>
+        					<p>Hai guys, I am Mambaur Roziq Alwi, usually called Bauroziq. I was born in Jember, Indonesia. I am very interested in the development of technology, especially in programming. I like to learn new things related to the latest technology in the world, such as frameworks, cloud computing, ai, and machine learning.</p>
         					<div class="row">
         						<div class="col-md-4">
         							<div class="wel_item">
@@ -135,15 +136,7 @@
         				<div class="tools_expert">
         					<div class="skill_main">
 								<div class="skill_item">
-									<h4>After Effects <span class="counter">85</span>%</h4>
-									<div class="progress_br">
-										<div class="progress">
-											<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-										</div>
-									</div>
-								</div>
-								<div class="skill_item">
-									<h4>Photoshop <span class="counter">90</span>%</h4>
+									<h4>Flutter <span class="counter">90</span>%</h4>
 									<div class="progress_br">
 										<div class="progress">
 											<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
@@ -151,26 +144,34 @@
 									</div>
 								</div>
 								<div class="skill_item">
-									<h4>Illustrator <span class="counter">70</span>%</h4>
+									<h4>Laravel <span class="counter">90</span>%</h4>
 									<div class="progress_br">
 										<div class="progress">
-											<div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+											<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
 										</div>
 									</div>
 								</div>
 								<div class="skill_item">
-									<h4>Sublime <span class="counter">95</span>%</h4>
+									<h4>PHP <span class="counter">85</span>%</h4>
 									<div class="progress_br">
 										<div class="progress">
-											<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+											<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
 										</div>
 									</div>
 								</div>
 								<div class="skill_item">
-									<h4>Sketch <span class="counter">75</span>%</h4>
+									<h4>Mysql <span class="counter">80</span>%</h4>
 									<div class="progress_br">
 										<div class="progress">
-											<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+											<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+									</div>
+								</div>
+								<div class="skill_item">
+									<h4>Javascript <span class="counter">85</span>%</h4>
+									<div class="progress_br">
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
 										</div>
 									</div>
 								</div>
@@ -337,72 +338,72 @@
         			<div class="col-lg-4 col-md-4 col-sm-6 brand manipul design print">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-1.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-1.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/ongkirku.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/ongkirku.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>3D Helmet Design</h4>
-        						<p>Client Project</p>
+        						<h4>Ongkirku</h4>
+        						<p>Cek Resi & Ongkos Kirim</p>
         					</div>
         				</div>
         			</div>
         			<div class="col-lg-4 col-md-4 col-sm-6 brand manipul creative">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-2.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-2.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/signature.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/signature.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>2D Vinyl Design</h4>
-        						<p>Client Project</p>
+        						<h4>Signature Maker</h4>
+        						<p>Simple Signature Maker</p>
         					</div>
         				</div>
         			</div>
         			<div class="col-lg-4 col-md-4 col-sm-6 manipul creative design print">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-3.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-3.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/alquran.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/alquran.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>Creative Poster Design</h4>
-        						<p>Client Project</p>
+        						<h4>Al-Quran App</h4>
+        						<p>Al-Quran Application</p>
         					</div>
         				</div>
         			</div>
         			<div class="col-lg-4 col-md-4 col-sm-6 brand creative print">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-4.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-4.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/doku.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/doku.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>Embosed Logo Design</h4>
-        						<p>Client Project</p>
+        						<h4>Dompet Saku</h4>
+        						<p>Aplikasi Catatan Keuangan</p>
         					</div>
         				</div>
         			</div>
         			<div class="col-lg-4 col-md-4 col-sm-6 brand manipul design">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-5.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-5.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/phonespec.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/phonespec.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>3D Disposable Bottle</h4>
-        						<p>Client Project</p>
+        						<h4>Phonespec</h4>
+        						<p>Phone Specification</p>
         					</div>
         				</div>
         			</div>
         			<div class="col-lg-4 col-md-4 col-sm-6 brand creative">
         				<div class="h_gallery_item">
         					<div class="g_img_item">
-        						<img class="img-fluid" src="{{url('/')}}/assets/img/gallery/project-6.jpg" alt="">
-        						<a class="light" href="{{url('/')}}/assets/img/gallery/project-6.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
+        						<img class="img-fluid" src="{{url('/')}}/assets/img/portofolio/recipe.jpg" alt="">
+        						<a class="light" href="{{url('/')}}/assets/img/portofolio/recipe.jpg"><img src="{{url('/')}}/assets/img/gallery/icon.png" alt=""></a>
         					</div>
         					<div class="g_item_text">
-        						<h4>3D Logo Design</h4>
-        						<p>Client Project</p>
+        						<h4>Indonesian Recipe</h4>
+        						<p>Indonesian Traditional Recipe</p>
         					</div>
         				</div>
         			</div>
